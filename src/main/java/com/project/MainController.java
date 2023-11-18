e8a2c50d21e10a903c530decc77aaf41093e422c
Endomou <christianmatthewevalla@gmail.com>
2023-11-18 12:45:55 +0800

    Added code

--- src/main/java/com/project/MainController.java
+++ src/main/java/com/project/MainController.java
@@ -1,36 +1,42 @@
 package com.project;
 
+import com.gluonhq.charm.glisten.control.CharmListView;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
-public class HelloController {
+/* Features to implement:
+    - ListView of Queue
+    - Sorting algorithms for queue
+    - Shuffled Playing
+
+Currently Being implemented but not working:
+    - Repeat feature
+    - Automatic next track after current song is finished
+ */
+public class MainController {
     @FXML
     private Text nowPlaying;
     Deque<Media> musicQueue = new LinkedList<>();
     Stack<Media> backStack = new Stack<>();
     MediaPlayer mediaPlayer;
     Boolean isPlaying=false;
+    @FXML
+    CharmListView<String,String> musicList;
+
+    Boolean isRepeat=false;
 
     public void initialize(){
-        File folder = new File("src/main/resources/assets/");
-        File[] listOfFiles = folder.listFiles();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            Media media = new Media( listOfFiles[i].toURI().toString());
-            musicQueue.add(media);
-        }
-        mediaPlayer = new MediaPlayer(musicQueue.peek());
-    }
 
+    }
 
     @FXML
     protected void playMusic(){
@@ -40,11 +46,18 @@ public class HelloController {
         }
 
         else{
-        mediaPlayer.play();
-        String title = mediaPlayer.getMedia().getMetadata().get("title").toString();
-        nowPlaying.setText(title);
-        isPlaying=true;
+            mediaPlayer.play();
+            String title = mediaPlayer.getMedia().getMetadata().get("title").toString();
+            nowPlaying.setText(title);
+            isPlaying=true;
         }
+
+        mediaPlayer.statusProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == MediaPlayer.Status.STOPPED) {
+                nextTrack();
+            }
+        });
+
     }
     @FXML
     protected void nextTrack(){
@@ -58,7 +71,6 @@ public class HelloController {
             isPlaying=true;
             String title= mediaPlayer.getMedia().getMetadata().get("title").toString();
             nowPlaying.setText(title);
-
 
         }
     }
@@ -74,7 +86,34 @@ public class HelloController {
             Media currentPlaying = mediaPlayer.getMedia();
             String title= mediaPlayer.getMedia().getMetadata().get("title").toString();
             nowPlaying.setText(title);
+
+
+        }
+}
+    @FXML
+    protected void addFolder() {
+        DirectoryChooser directory = new DirectoryChooser();
+        File selectedFolder = directory.showDialog(new Stage());
+        if(selectedFolder!=null){
+        File[] listOfFiles = selectedFolder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            Media media = new Media( listOfFiles[i].toURI().toString());
+            musicQueue.add(media);
+        }
+        mediaPlayer = new MediaPlayer(musicQueue.peek());
+        }
+    }
+
+    @FXML
+    protected void setRepeat(){
+        if(isRepeat){
+            isRepeat=false;
+        }
+        else{
+            isRepeat=true;
         }
 
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.project {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-
+    requires charm.glisten;
 
     opens com.project to javafx.fxml;
     exports com.project;
